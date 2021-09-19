df5c84daf9771bb13a3677a88b23da3ef659dcd5
EstebanDSM <stephanedasil@gmail.com>
2021-09-19 19:19:13 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            // Here we take the value from the MyHomePage object that was created by
+            // the App.build method, and use it to set our appbar title.
+            title: const Text("mes recettes"),
+          ),
+          body: Column(
+            children: [
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                          padding: const EdgeInsets.only(bottom: 8),
+                          child: const Text('pizza facile',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20))),
+                      Text("Stef DS",
+                          style:
+                              TextStyle(color: Colors.grey[700], fontSize: 20))
+                    ],
+                  ),
+                  Icon(Icons.favorite, color: Colors.red),
+                  Text("55")
+                ],
+              ),
+            ],
+          )),
+    );
+  }
+}
