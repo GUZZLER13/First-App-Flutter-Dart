f85947235df800ae7b6d343750472198e70b3ed4
EstebanDSM <stephanedasil@gmail.com>
2021-09-19 21:34:34 +0200

    add row text

--- lib/main.dart
+++ lib/main.dart
@@ -4,42 +4,62 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-
-   Widget titleSection = (Container(
-    padding: const EdgeInsets.all(8),
-    child: Row(
-      children: [
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text('pizza facile',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 20))),
-              Text("Stef DS",
-                  style: TextStyle(color: Colors.grey[700], fontSize: 20))
-            ],
-          ),
-        ),
-        const Icon(Icons.favorite, color: Colors.red),
-        const Text("55")
-      ],
-    ),
-  ));
-
-
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Widget buttonSection = Container(
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(Colors.blue, Icons.comment, "COMMENT"),
+          _buildButtonColumn(Colors.blue, Icons.share, "SHARE"),
+        ],
+      ),
+    );
+
+    Widget titleSection = (Container(
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: const Text('pizza facile',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20))),
+                Text("Stef DS",
+                    style: TextStyle(color: Colors.grey[700], fontSize: 20))
+              ],
+            ),
+          ),
+          const Icon(Icons.favorite, color: Colors.red),
+          const Text("55")
+        ],
+      ),
+    ));
+
+    Widget descriptionSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        "Per hoc minui studium suum existimans "
+            "Paulus, ut erat in conplicandis negotiis artifex dirus, unde ei Catenae inditum est cognomentum, vicarium ipsum eos quibus praeerat adhuc defensantem ad sortem "
+            "periculorum communium traxit. et instabat ut eum quoque cum tribunis et aliis pluribus ad comitatum imperatoris vinctum perduceret: quo percitus ille exitio urgente "
+            "abrupto ferro eundem adoritur Paulum. et quia languente dextera, letaliter ferire non potuit, iam districtum mucronem in proprium latus inpegit. hocquedeformi genere "
+            "mortis excessit e vita iustissimus rector ausus miserabiles casus levare multorum.",
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -61,8 +81,25 @@ class MyApp extends StatelessWidget {
             title: const Text("mes recettes"),
           ),
           body: Column(
-            children: [titleSection],
+            children: [titleSection, buttonSection, descriptionSection],
           )),
     );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Container(
+        padding: const EdgeInsets.only(bottom: 8),
+        child: Icon(icon, color: color),
+      ),
+      Text(
+        label,
+        style: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          color: color,
+        ),
+      )
+    ]);
   }
 }
